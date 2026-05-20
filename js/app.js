66f5cc84865a872616a69da89dcf6de717f90702
namitharaman <namitharaman741@gmail.com>
2026-05-20 11:59:42 +0530

    Initial commit -healthcare

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function bookAppointment(){
+
+    document.getElementById("appointmentMessage")
+    .innerHTML =
+    "Appointment Booked Successfully";
+
+}
+
+function emergencyCall(){
+
+    document.getElementById("emergencyMessage")
+    .innerHTML =
+    "Calling Emergency Number 108";
+
+}
